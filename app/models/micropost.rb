6f6b8f768099841ab0c9c746d95068536a91e441
itzik <eitzikgr@gmail.com>
2013-06-13 16:40:13 +0300

    kokoriko

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   #validates:content; lenght=>{:maximum=>50}
-  validates :content, :length => { :maximum => 15 }
+  validates :content, :length => { :maximum => 140 }
   validates :content, :length => { :minimum => 5 }
   #belongs_to: user
 end
